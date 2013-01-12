1b478ad47f70c01554d6f429cd83467bc3bb2edd
satoru <satorulogic@gmail.com>
2013-01-12 20:08:15 +0800

    支持列表模式

--- src/doulist.js
+++ src/doulist.js
@@ -3,11 +3,36 @@
     var subjectEls = $(".article .item");
     var bookId2Els = {};
     var bookIds = [];
+    var isListMode = window.location.search.match(/mode=list/);
+
+    if (!isListMode) {
+        function getBookId(item) {
+            var bookURL = item.find(".title a").attr('href');
+            return bookURL.match(/\d+/)[0];
+        }
+
+        function updatePage(bid, res) {
+            var el = bookId2Els[bid];
+            var target = el.find("li.intro + li");
+            target.append('<a class="ikd-lnk" target="_blank" href="' +
+                            res.url + '">爱看豆</a>');
+        }
+    } else {
+        function getBookId(item) {
+            return item.attr('id').match(/\d+/)[0];
+        }
+
+        function updatePage(bid, res) {
+            var el = bookId2Els[bid];
+            var target = el.find(".title");
+            target.append('<a class="ikd-lnk" target="_blank" href="' +
+                            res.url + '">爱看豆</a>');
+        }
+    }
 
     subjectEls.each(function () {
         var el = $(this);
-        var bookURL = el.find(".title a").attr('href');
-        var bookId = bookURL.match(/\d+/)[0];
+        var bookId = getBookId(el);
         bookId2Els[bookId] = el;
         bookIds.push(bookId);
     })
@@ -21,10 +46,7 @@
            var result = JSON.parse(xhr.response);
            $.each(result, function (bid, ob) {
                if (ob) {
-                   var el = bookId2Els[bid];
-                   var target = el.find("li.intro + li");
-                   target.append('<a class="ikd-lnk" target="_blank" href="' +
-                                   ob.url + '">爱看豆</a>');
+                   updatePage(bid, ob);
                }
            });
 
